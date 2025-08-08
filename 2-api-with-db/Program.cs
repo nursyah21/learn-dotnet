fc9d4f616e78304b33fbcded3e7bf1f10d3866c6
nursyah21 <nursyahjaya@gmail.com>
2025-08-09 07:30:11 +0800

    add openapi and https redirection

--- 2-api-with-db/Program.cs
+++ 2-api-with-db/Program.cs
@@ -2,12 +2,16 @@ using _2_api_with_db.Services.Implementations;
 using _2_api_with_db.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IStudentService, StudentService>();
 
 var app = builder.Build();
 
+// https://localhost:5001/openapi/v1.json
+app.MapOpenApi();
 app.MapControllers();
 
+app.UseHttpsRedirection();
 
-app.Run("http://localhost:5001");
+app.Run("https://localhost:5001");
