8675745c338382d6ad9560267f9273110dfc426d
nursyah21 <nursyahjaya@gmail.com>
2025-08-09 08:44:35 +0800

    database migrations

--- 2-api-with-db/Program.cs
+++ 2-api-with-db/Program.cs
@@ -1,17 +1,25 @@
+using _2_api_with_db.Data.Context;
 using _2_api_with_db.Services.Implementations;
 using _2_api_with_db.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Service Registration
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresqlConnection")));
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<IStudentService, StudentService>();
 
 var app = builder.Build();
 
+// Middleware
+app.UseHttpsRedirection();
+
 // https://localhost:5001/openapi/v1.json
 app.MapOpenApi();
 app.MapControllers();
 
-app.UseHttpsRedirection();
 
 app.Run("https://localhost:5001");
